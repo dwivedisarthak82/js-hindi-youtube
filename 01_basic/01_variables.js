cf20eeb70da4776c6b8c21d6a30391cd1e12fa21
Sarthak dwivedi <166862654+dwivedisarthak82@users.noreply.github.com>
2024-10-07 08:05:30 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144354
+let accountEmail="diwvedisarthak"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountstate;
+//accountId=2
+accountEmail="hdf@gmail.com"
+accountPassword="12223445"
+accountCity="delhi"
+console.log(accountId);
+/*
+prefer not to use var
+because of issue in block and functional scope
+*/
+console.table([accountId,accountPassword,accountEmail,accountCity,accountstate]);
