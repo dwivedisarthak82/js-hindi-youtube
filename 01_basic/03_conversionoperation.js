c91cdf50c28aacc2c83112bf15fb5a2745d29493
Sarthak dwivedi <166862654+dwivedisarthak82@users.noreply.github.com>
2024-10-10 12:36:39 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,24 @@
+let score= 33
+console.log(typeof score);
+let score1="33avs"
+console.log(typeof score1);
+let valueinnumber= Number(score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+let valuenumber=Number(score1)
+console.log(typeof valuenumber);
+console.log(valuenumber);
+
+//"33" easiliy convert
+//33avjs nan
+//true=1 false =0
+let isloggedin="sarthak"
+let booleanisloggedin =Boolean(isloggedin)
+console.log(booleanisloggedin);
+// 1=true 0=false
+//"" =false
+//"sarthak"=true
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
