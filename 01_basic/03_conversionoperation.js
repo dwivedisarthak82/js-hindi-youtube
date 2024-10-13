e6513c3ed3a7a3a264afe05a114f22b4278f76ec
Sarthak dwivedi <166862654+dwivedisarthak82@users.noreply.github.com>
2024-10-13 17:53:03 +0000

    conversion operation done

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -22,3 +22,36 @@ let somenumber=33
 let stringnumber=String(somenumber)
 console.log(stringnumber);
 console.log(typeof stringnumber);
+
+//************************** operation***********
+
+let value =3
+let negvalue =-value
+console.log(negvalue);
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2/2)
+//console.log(2**3)
+//console.log(2%2)
+
+let str1 ="hitesh"
+let str2 ="chudhary"
+let str3 =str1+str2
+console.log(str3);
+
+console.log("1" +2)
+console.log(1 +"2")
+console.log("1" +2 +2)
+console.log(1 +2 +"2")
+
+console.log(true);
+console.log(+true);
+
+let num1 ,num2 ,num3
+num1= num2= num3=2+2
+
+let d=100;
+d++;
+console.log(d);
